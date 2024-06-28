d1dc6a90d3f1fc4f4f17e587792afb16eda54c45
stefana <theanafets@gmail.com>
2024-06-28 20:37:47 +0200

    Client

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () =>{
+    return(
+        <header>
+            <h2><Link to='/'>Workout Buddy</Link></h2>
+        </header>
+    )
+}
+
+export default Navbar
